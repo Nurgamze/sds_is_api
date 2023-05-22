a674b0e4b659bedff8128fbb190a006a28ddf0b5
Nurgamze <gamzenurdemir1>
2023-05-22 11:24:42 +0300

    add new pages

--- lib/screens/pozisyonDetay.dart
+++ lib/screens/pozisyonDetay.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'homePage.dart';
+
 class BasvuruPage extends StatefulWidget {
-  const BasvuruPage({Key? key}) : super(key: key);
+  final String email;
+  final String password;
+  const BasvuruPage({Key? key, required this.email, required this.password}) : super(key: key);
 
   @override
   State<BasvuruPage> createState() => _BasvuruPageState();
@@ -16,6 +20,12 @@ class _BasvuruPageState extends State<BasvuruPage> {
         title: Text("Başvuru Sayfası"),
         centerTitle: true,
         backgroundColor: Colors.brown,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: (){
+            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomePage(email: widget.email, password: widget.password,)));
+          },
+        ),
       ),
     );
   }
